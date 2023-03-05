f226ddf1e60e818be8f00d1dfb1a415f485fbc69
Muhamad hamza saleem <64617903+hamzasaleem2397@users.noreply.github.com>
2023-03-06 02:17:32 +0500

    updated

---
+++ src/portfolio.js
@@ -0,0 +1,566 @@
+/* Change this file to get your personal Porfolio */
+
+// Website related settings
+const settings = {
+  isSplash: true, // Change this to true if you want to use the splash screen.
+  useCustomCursor: true, // Change this to false if you want the good'ol cursor
+  googleTrackingID: "UA-174238252-2",
+};
+
+//Home Page
+const greeting = {
+  title: "Hello 👋.",
+  title2: "Muhammad Hamza Saleem",
+  logo_name: "hamza.()",
+  nickname: "Hmxa",
+  full_name: "Muhammad Hamza Saleem",
+  subTitle: "React / ReactNative Developer 🔥. Always learning.",
+  resumeLink:
+    "https://docs.google.com/document/d/1A_jWRG74Rst427tg1izLa6vRXclS9_9F856jWJPJlDY/edit?usp=sharing",
+  mail: "mailto:hamzasaleem2397@gmail.com",
+};
+
+const socialMediaLinks = {
+  /* Your Social Media Link */
+  github: "https://github.com/hamzasaleem2397",
+  linkedin: "https://www.linkedin.com",
+  gmail: "hamzasaleem2397@gmail.com",
+  facebook: "https://www.facebook.com",
+  instagram: "https://www.instagram.com/hari_kanani_/",
+};
+
+const skills = {
+  data: [
+    {
+      title: "Full Stack Development",
+      fileName: "FullStackImg",
+      skills: [
+        "⚡ Develop highly interactive Front end / User Interfaces for your web and mobile applications",
+        "⚡ Building responsive website front end using ReactJS",
+        "⚡ Developing mobile applications using React Native",
+        "⚡ Creating application backend in Node, Express & Mongodb",
+      ],
+      softwareSkills: [
+        {
+          skillName: "HTML5",
+          fontAwesomeClassname: "simple-icons:html5",
+          style: {
+            color: "#E34F26",
+          },
+        },
+        {
+          skillName: "CSS3",
+          fontAwesomeClassname: "fa-css3",
+          style: {
+            color: "#1572B6",
+          },
+        },
+        {
+          skillName: "JavaScript",
+          fontAwesomeClassname: "simple-icons:javascript",
+          style: {
+            backgroundColor: "#FFFFFF",
+            color: "#F7DF1E",
+          },
+        },
+
+        {
+          skillName: "ReactJS",
+          fontAwesomeClassname: "simple-icons:react",
+          style: {
+            color: "#61DAFB",
+          },
+        },
+        {
+          skillName: "NodeJS",
+          fontAwesomeClassname: "simple-icons:node-dot-js",
+          style: {
+            color: "#339933",
+          },
+        },
+        {
+          skillName: "NPM",
+          fontAwesomeClassname: "simple-icons:npm",
+          style: {
+            color: "#CB3837",
+          },
+        },
+        {
+          skillName: "MongoDB",
+          fontAwesomeClassname: "simple-icons:mongodb",
+          style: {
+            color: "#439743",
+          },
+        },
+        // {
+        //   skillName: "GraphQL",
+        //   fontAwesomeClassname: "simple-icons:graphql",
+        //   style: {
+        //     color: "#DE33A6",
+        //   },
+        // },
+
+        // {
+        //   skillName: "Visual Basic",
+        //   fontAwesomeClassname: "simple-icons:dot-net",
+        //   style: {
+        //     color: "#029FCE",
+        //   },
+        // },
+
+        {
+          skillName: "jQuery",
+          fontAwesomeClassname: "simple-icons:jquery",
+          style: {
+            color: "#0865A6",
+          },
+        },
+
+        {
+          skillName: "Git",
+          fontAwesomeClassname: "simple-icons:git",
+          style: {
+            color: "#E94E32",
+          },
+        },
+      ],
+    },
+  ],
+};
+
+const degrees = {
+  degrees: [
+    {
+      title: "Bahria University Karachi Campus",
+      subtitle: "Bachelor in Computer Science",
+      logo_path: "ssec.png",
+      alt_name: "SSEC",
+      duration: "2021 - Present",
+      descriptions: [
+        "⚡ I've completed my Bachelor degree in 2021.",
+        "⚡ I have studied core subjects like Data Structures, DBMS, Networking, Security, etc.",
+        "⚡ I have also completed various online courses for Backend , Web , Mobile App Development, etc.",
+        "⚡ I have worked on various mobile applications which are live in playstore and appstore ",
+      ],
+      website_link: "https://www.bahria.edu.pk/",
+    },
+  ],
+};
+
+const certifications = {
+  certifications: [
+    {
+      title: "Introduction to Game developement",
+      subtitle: "Michigen University from Coursera",
+      logo_path: "mongo.png",
+      certificate_link:
+        "https://drive.google.com/file/d/12se6bMep48WXA1PlizTBAgCnW0GXM6wq/view?usp=sharing",
+      alt_name: "Coursera",
+      // color_code: "#2AAFED",
+      color_code: "#47A048",
+    },
+    // color_code: "#b190b0",
+  ],
+};
+
+// Experience Page
+const experience = {
+  title: "Experience",
+  subtitle: "Work, Internship and Volunteership",
+  description:
+    "I've completed two internships.Currently working as a React Native Developer in Visech Technology.",
+  header_image_path: "experience.svg",
+  sections: [
+    {
+      title: "Work Experience",
+      experiences: [
+        {
+          title: "React Native Developer",
+          company: "Sas Solutions",
+          company_url: "https://valora-infotech.business.site/",
+          logo_path: "valora.jpg",
+          duration: "Jan 2022 - Oct 2022",
+          location: "Office",
+          description: `Train, manage and provide guidance to junior software development staff.
+          Work closely with the Project Manager and Team Leads on change request functions.
+          Develops new and maintains existing applications.
+          Increased productivity and problem-solving technics by 20%.
+          We primarily used PERN and MERN Stack. 80% of my work was doing backend development and the remaining 20% was DevOps-related tasks.
+          `,
+          // "I worked on the Dashboard project which helps users track their activities while using Walo Application. I also worked on Ocean Inventory Application and it's Admin panel Backend as well as on Ocean Inventory Admin Front-end using React and also worked on Walo Admin Backend.",
+          color: "#0071C5",
+        },
+        {
+          title: "React Native Developer",
+          company: "Visech Technolgy",
+          company_url: "http://nightowls.company/",
+          logo_path: "nightowls.jpg",
+          duration: "Sep 2020 - Oct 2020",
+          location: "Office",
+          description: `Work closely with the Client and Team on change request functions.
+          We Primarily used LAMP Stack. 40% of my work was doing backend development, 30% of my work was designing database schemas, and 30% of my work was on maintain hosting and user traffics on the website.
+          `,
+          // "Created Front end of Yearn Financial Mutual Funds website. also degined simple web application for better user experience, designed DB Schemas as well.",
+          color: "#ee3c26",
+        },
+      ],
+    },
+  ],
+};
+
+// Projects Page
+const projectsHeader = {
+  title: "Projects",
+  description:
+    "My projects make use of a vast variety of latest technology tools. My best experience is to create NodeJS Backend Projects, Python Scripts, and React Project. Below are some of my projects. Note that not all of the mentioned projects are on GitHub yet.",
+  avatar_image_path: "projects_image.svg",
+};
+
+// Contact Page
+const contactPageData = {
+  contactSection: {
+    title: "Contact Me",
+    profile_image_path: "harikrushn.jpg",
+    description:
+      "You can contact me at the places mentioned below. I will try to get back to you as fast as I can. ",
+  },
+  blogSection: {
+    title: "Blogs",
+    subtitle:
+      "I don't blog frequently but when I do something awesome, I do try to document it so it can be helpful to others. I write on Twitter.",
+    link: "https://twitter.com/Harikrushn9",
+    avatar_image_path: "blogs_image.svg",
+  },
+};
+
+const projects = {
+  data: [
+    {
+      id: "0",
+      name: "AutomateInstaPyBot",
+      url: "https://github.com/harikanani/AutomateInstaPyBot",
+      description:
+        "This is Instagram Bot. This will login to your Instagram account. Follow Users, Unfollow Users, Remove Profile Photo",
+      languages: [
+        {
+          name: "Python",
+          iconifyClass: "logos-python",
+        },
+      ],
+    },
+    {
+      id: "1",
+      name: "react-twitter-clone",
+      url: "https://github.com/harikanani/react-twitter-clone",
+      description:
+        "A React Twitter Clone UI with basic functionality such as make a Tweet.Embedded Profile Tweets and Share on Twitter.",
+      languages: [
+        {
+          name: "HTML5",
+          iconifyClass: "vscode-icons:file-type-html",
+        },
+        {
+          name: "CSS3",
+          iconifyClass: "vscode-icons:file-type-css",
+        },
+        {
+          name: "React",
+          iconifyClass: "logos-react",
+        },
+        {
+          name: "Firebase",
+          iconifyClass: "logos-firebase",
+        },
+      ],
+    },
+    {
+      id: "2",
+      name: "node-blockchain",
+      url: "https://github.com/harikanani/node-blockchain",
+      description:
+        "A simple blockchain and cryptocurrency wallet implemented in Node.js and TypeScript (for learning purposes).",
+      languages: [
+        {
+          name: "NodeJS",
+          iconifyClass: "logos-nodejs",
+        },
+        {
+          name: "TypeScript",
+          iconifyClass: "logos-typescript",
+        },
+      ],
+    },
+    {
+      id: "3",
+      name: "top-crypto-gainers",
+      url: "https://github.com/harikanani/top-crypto-gainers",
+      description:
+        "A top high price changed crypto coins wring 24 hoursA sound-classifier webapp made with ReactJS + TensorflowJS.",
+      languages: [
+        {
+          name: "HTML5",
+          iconifyClass: "vscode-icons:file-type-html",
+        },
+        {
+          name: "CSS3",
+          iconifyClass: "vscode-icons:file-type-css",
+        },
+        {
+          name: "JavaScript",
+          iconifyClass: "logos-javascript",
+        },
+        {
+          name: "ReactJS",
+          iconifyClass: "logos-react",
+        },
+      ],
+    },
+    {
+      id: "4",
+      name: "personal-portfolio",
+      url: "https://github.com/harikanani/personal-portfolio",
+      description:
+        "A simple command line interface based quiz app to know more about me :).",
+      languages: [
+        {
+          name: "JavaScript",
+          iconifyClass: "logos-javascript",
+        },
+        {
+          name: "NodeJS",
+          iconifyClass: "logos-nodejs",
+        },
+      ],
+    },
+    {
+      id: "3",
+      name: "node_express_crud_api_starter",
+      url: "https://github.com/harikanani/node_express_crud_api_starter",
+      description: "Simple NodeJS Express CRUD Operations API starter.",
+      languages: [
+        {
+          name: "NodeJS",
+          iconifyClass: "logos-nodejs",
+        },
+      ],
+    },
+    {
+      id: "4",
+      name: "node-web-scrapper",
+      url: "https://github.com/harikanani/node-web-scrapper",
+      description:
+        "A Simple web scrapper that scrap the information of amazon products such as price.It also scrap Wikipedia Data such as birthdate.",
+      languages: [
+        {
+          name: "HTML5",
+          iconifyClass: "vscode-icons:file-type-html",
+        },
+        {
+          name: "CSS3",
+          iconifyClass: "vscode-icons:file-type-css",
+        },
+        {
+          name: "NodeJs",
+          iconifyClass: "logos-nodejs",
+        },
+      ],
+    },
+    {
+      id: "5",
+      name: "harikanani.github.io",
+      url: "https://github.com/harikanani/harikanani.github.io",
+      description:
+        "A Personal Portfolio Website that showcases my work and experience. which is hosted on Github Pages.",
+      languages: [
+        {
+          name: "HTML5",
+          iconifyClass: "vscode-icons:file-type-html",
+        },
+        {
+          name: "CSS3",
+          iconifyClass: "vscode-icons:file-type-css",
+        },
+        {
+          name: "JavaScript",
+          iconifyClass: "logos-javascript",
+        },
+        {
+          name: "SCSS",
+          iconifyClass: "vscode-icons:file-type-scss2",
+        },
+      ],
+    },
+    {
+      id: "6",
+      name: "Automate Attendace",
+      url: "https://github.com/harikanani/Node_Python",
+      description: "Automation of Online Attendance using node js and python",
+      languages: [
+        {
+          name: "JavaScript",
+          iconifyClass: "logos-javascript",
+        },
+        {
+          name: "Python",
+          iconifyClass: "logos-python",
+        },
+      ],
+    },
+    {
+      id: "7",
+      name: "Automate Discord Bot",
+      url: "https://github.com/harikanani/AutomateDiscordBot",
+      description:
+        "A Discord Bot to send Automatic messages to serer channel in regular time difference.",
+      languages: [
+        {
+          name: "Python",
+          iconifyClass: "logos-python",
+        },
+        {
+          name: "Python Selenium",
+          iconifyClass: "logos-selenium",
+        },
+        {
+          name: "Chromium Browser",
+          iconifyClass: "openmoji-chromium",
+        },
+      ],
+    },
+    {
+      id: "8",
+      name: "Flask Blog",
+      url: "https://github.com/harikanani/flask_blog",
+      description: "A Simple Blog Web Application made using Flask Framework",
+      languages: [
+        {
+          name: "Python",
+          iconifyClass: "logos-python",
+        },
+        {
+          name: "Flask",
+          iconifyClass: "cib-flask",
+        },
+        {
+          name: "HTML5",
+          iconifyClass: "vscode-icons:file-type-html",
+        },
+        {
+          name: "CSS3",
+          iconifyClass: "vscode-icons:file-type-css",
+        },
+      ],
+    },
+    {
+      id: "9",
+      name: "Netflix top series",
+      url: "https://github.com/harikanani/netflix-top-series",
+      description: "List of Top Netflix Series which is deployed to vercel.",
+      languages: [
+        {
+          name: "JavaScript",
+          iconifyClass: "logos-javascript",
+        },
+        {
+          name: "ReactJS",
+          iconifyClass: "logos-react",
+        },
+        {
+          name: "HTML5",
+          iconifyClass: "logos-html-5",
+        },
+        {
+          name: "CSS3",
+          iconifyClass: "logos-css-3",
+        },
+      ],
+    },
+    {
+      id: "10",
+      name: "COVID-19 Tracker",
+      url: "https://github.com/harikanani/Covid19TrackerReact",
+      description:
+        "Simple Covid-19 Tracker made using React and deployed to Vercel.",
+      languages: [
+        {
+          name: "JavaScript",
+          iconifyClass: "logos-javascript",
+        },
+        {
+          name: "ReactJS",
+          iconifyClass: "logos-react",
+        },
+        {
+          name: "HTML5",
+          iconifyClass: "logos-html-5",
+        },
+        {
+          name: "CSS3",
+          iconifyClass: "logos-css-3",
+        },
+      ],
+    },
+    {
+      id: "11",
+      name: "Food Order Static Website",
+      url: "https://github.com/harikanani/food-order-website",
+      description:
+        "A simple static website related to food restaurants service. this is reasponsive as well.",
+      languages: [
+        {
+          name: "HTML5",
+          iconifyClass: "logos-html-5",
+        },
+        {
+          name: "CSS3",
+          iconifyClass: "logos-css-3",
+        },
+        {
+          name: "JavaScript",
+          iconifyClass: "logos-javascript",
+        },
+        {
+          name: "PHP",
+          iconifyClass: "logos-php",
+        },
+      ],
+    },
+    {
+      id: "12",
+      name: "NFT Launchpad",
+      url: "https://deliquescent-cents.000webhostapp.com/",
+      description: "NFT Launchpad crypto site portfolio",
+      languages: [
+        {
+          name: "HTML5",
+          iconifyClass: "logos-html-5",
+        },
+        {
+          name: "CSS3",
+          iconifyClass: "logos-css-3",
+        },
+        {
+          name: "JavaScript",
+          iconifyClass: "logos-javascript",
+        },
+        {
+          name: "ReactJS",
+          iconifyClass: "logos-react",
+        },
+      ],
+    },
+  ],
+};
+
+export {
+  settings,
+  greeting,
+  socialMediaLinks,
+  skills,
+  degrees,
+  certifications,
+  experience,
+  projectsHeader,
+  contactPageData,
+  projects,
+};
